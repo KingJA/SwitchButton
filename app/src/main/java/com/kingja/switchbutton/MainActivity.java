808625f5937d9d8cc6af29aaf80094723b8a25e3
KingJA <kingjavip@gmail.com>
2016-07-19 17:12:41 +0800

    clean

--- app/src/main/java/com/kingja/switchbutton/MainActivity.java
+++ app/src/main/java/com/kingja/switchbutton/MainActivity.java
@@ -5,8 +5,6 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "MainActivity";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
