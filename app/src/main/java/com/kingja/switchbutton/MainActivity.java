d0c225c4b5356f000e4ec929e65440495d548816
KingJA <kingjavip@gmail.com>
2016-08-12 09:33:48 +0800

    build 09:30

--- app/src/main/java/com/kingja/switchbutton/MainActivity.java
+++ app/src/main/java/com/kingja/switchbutton/MainActivity.java
@@ -33,7 +33,7 @@ import lib.kingja.switchbutton.SwitchMultiButton;
  * Create Time：2016/7/27 10:26
  * Author:KingJA
  * Email:kingjavip@gmail.com
- * update:add into Jenkins 15:35
+ * update:add into Jenkins 09:30
  */
 public class MainActivity extends AppCompatActivity {
 
